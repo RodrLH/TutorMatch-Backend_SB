5bf8bf689d25eef67c7e5c23de8f67d3a30968b1
Harold Elías <129306594+HamesEl@users.noreply.github.com>
2024-11-14 22:29:33 -0500

    primer cambio main

--- src/main/java/com/skillswap/platform/tutormatch/TutorMatchApplication.java
+++ src/main/java/com/skillswap/platform/tutormatch/TutorMatchApplication.java
@@ -2,7 +2,11 @@ package com.skillswap.platform.tutormatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * TutorMatchApplication
@@ -20,6 +24,19 @@ public class TutorMatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorMatchApplication.class, args);
+	}
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
